f0516e7cfff7b9ff390c91ea7130dfb5b0cc72bb
Salah eddine <abw.salaheddine@gmail.com>
2026-02-17 20:17:33 +0000

    fixes

--- src/pages/Analytics.tsx
+++ src/pages/Analytics.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import { BarChart2 } from "lucide-react";
 
 export default function Analytics() {
--- src/pages/Integrations.tsx
+++ src/pages/Integrations.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import { Blocks } from "lucide-react";
 
 export default function Integrations() {
